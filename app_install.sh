c3be26f4c2d18543c06a33754b4b98a56211fed4
marinaviana <marina.s.viana@gmail..com>
2021-09-21 22:40:20 -0300

    add repo

---
+++ app_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu -o pipefail # fail on error , debug all lines
+
+sudo -n true
+test $? -eq 0 || exit 1 "you should have sudo priveledge to run this script"
+
+echo installing the must-have pre-requisites
+while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
+    git
+    curl
+    wget
+EOF
+)
+
+sudo snap install --classic code
+
+echo "FIM"
